dc0816024373c3d6313e17075b985ac5679022e8
mawedi <106656491+mawedi@users.noreply.github.com>
2024-05-04 17:12:17 +0100

    Update DataTrain.py

--- DataTraining/DataTrain.py
+++ DataTraining/DataTrain.py
@@ -1,10 +1,1 @@
-from sklearn.neighbors import KNeighborsClassifier
-
-# Training the model on providing the variable k
-def train_model(X_train, Y_train, n_neighbors):
-    model = KNeighborsClassifier(n_neighbors=n_neighbors)
-    model.fit(X_train, Y_train)
-
-    return model
-
 
